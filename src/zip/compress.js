fe9b8c6bc01b23432d466c7abc0f21e31a2b2b59
Aleksandr <aleksandreliuz@gmail.com>
2022-06-05 18:05:47 +0300

    feat: add functionality for the files in the modules and zip filders

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,15 @@
+import fs from 'fs'
+import zlib from 'zlib'
+
 export const compress = async () => {
-    // Write your code here 
+    
+let readableStream = fs.createReadStream("./files/fileToCompress.txt", "utf8");
+  
+let writeableStream = fs.createWriteStream("./files/archive.gz");
+  
+let gzip = zlib.createGzip();
+  
+readableStream.pipe(gzip).pipe(writeableStream);
 };
+
+compress();
