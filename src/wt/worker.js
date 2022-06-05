37aa248761d5f7a44e25ea7766dc02fba73a64f4
Aleksandr <aleksandreliuz@gmail.com>
2022-06-05 23:13:17 +0300

    feat: add solution for the Worker Threads module

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,25 @@
+import {workerData, parentPort} from 'worker_threads';
+let result = {
+    status: null,
+    data: null
+}
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+parentPort.on('message',(n)=>{
+    try{
+        result.data = nthFibonacci(n);
+        result.status = 'resolved';
+    }
+    catch{
+        result.data = null;
+        result.status = 'error'
+     }
+    sendResult();
+});
+
+
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+   parentPort.postMessage(result);
+   //console.log(result)
 };
